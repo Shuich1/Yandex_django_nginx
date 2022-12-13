097639a540c1ad4eb6f7db7909d2323521f8ff00
Shuichi <zhuravkov.mai@gmail.com>
2022-12-13 15:42:45 +0300

    Added database waiting

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,6 +2,10 @@
 # Collect static files
 python manage.py collectstatic --noinput
 # Apply database migrations
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+      echo "Waiting for database..."
+done 
 python manage.py migrate
 # Start server
 uwsgi --strict --ini uwsgi.ini
